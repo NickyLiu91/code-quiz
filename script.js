443772a02a4ee2db9a04d5d3d12887efcca04483
Fri Test <mr.alvarofrias@gmail.com>
2021-09-25 14:24:16 -0700

    starting to add content to javascript file

---
+++ script.js
@@ -0,0 +1,27 @@
+//array for questions and anwsers. Total will maybe be 5-10
+
+var quizQuestions = [
+    {
+        question: "what is javascript?",
+        choices: "choices",
+        answer: "answer coming soon",
+    }
+    question: "what is javascript?",
+    choices: "choices",
+    answer: "answer coming soon",
+        }
+        question: "what is javascript?",
+        choices: "choices",
+        answer: "answer coming soon",
+    }
+    question: "what is javascript?",
+    choices: "choices",
+    answer: "answer coming soon",
+}
+question:"what is javascript?",
+    choices: "choices",
+    answer: "answer coming soon",
+}
+
+
+];
