0ce454b76edff8ae75f95a3a9e69c8da306491e7
Fri Test <mr.alvarofrias@gmail.com>
2021-09-25 20:46:36 -0700

    change buttons from ID to class to in HTML

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-
-
-
-
-
-//declare variables
-var clickingTimer = document.querySelector(".timer-count");
+//declare variables for elements
+var clickBegin = document.querySelector(".header-button");
 var buttonInitiator = document.querySelector(".button-initiator");
+var quizResult = document.querySelector(".quiz-results");
+var scoreCard = document.querySelector(".corect-incorect-section");
+var ScoreCor = document.querySelector(".correct");
+var ScoreIncor= document.querySelector("incorrect");
+var resetQuiz = document.querySelector(".reset-button");
+var quizTimer = document.querySelector(".reset-button");
+var timeText = document.querySelector(".timer-text");
+var timeCount = document.querySelector(".timer-count");
+var checkScore = document.querySelector(".score-button");
 
 var timerCount;
 
@@ -15,5 +19,5 @@ timerCount = 180;
 
 
 }
-
+//adding the click event listener to to begin quiz button
 buttonInitiator.addEventListener("click", beginQuiz);
